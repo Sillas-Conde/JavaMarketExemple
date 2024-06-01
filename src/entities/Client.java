4214f54282b1e829b2048ed2cd56f4794c19b12c
sillasconde <sillasconde@usp.br>
2024-06-01 17:23:42 -0300

    Upload Java market first project

---
+++ src/entities/Client.java
@@ -0,0 +1,60 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Client {
+	String name;
+	String email;
+	Date birthDate;
+	
+	//Order order;
+	
+	public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Client(String name, String email, Date birthDate) {
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+	
+	
+	
+	
+	public String toString() {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(this.getName() + " ");
+		sb.append("(" + sdf.format(this.getBirthDate()) + ")");
+		sb.append(" - ");
+		sb.append(this.getEmail());
+		
+		return sb.toString();
+	}
+	
+}
